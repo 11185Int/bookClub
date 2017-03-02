e57adab149acfaf188c300c573f5c04c2849d2fe
Keson <longlongxs@gmail.com>
2017-03-02 16:37:05 +0800

    add access log

---
+++ server/src/middlewares/logger.php
@@ -0,0 +1,31 @@
+<?php
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+use CP\common\AccountSessionKey;
+
+$app->add(function (Request $request, Response $response, $next) {
+    //BEFORE
+
+    //NEXT
+    $response = $next($request, $response);
+    //AFTER
+
+    $logger = new \CP\common\Logger();
+    $client_ip = $logger->getClientIp();
+    $uri = $request->getUri();
+    $account = new AccountSessionKey();
+    $openid = $account->getOpenIdByKey($request->getParam('key'));
+    $body = $response->getBody();
+    $logData = [
+        'action' => $uri->getPath(),
+        'data' => $uri->getQuery(),// json_encode($request->getQueryParams()),
+        'posttime' => date('Y-m-d H:i:s', time()),
+        'openid' => $openid ?: '',
+        'returndata' => addslashes((string)$body),
+        'userip' => $client_ip,
+    ];
+    $logger->save($logData);
+
+    return $response;
+});
