7ec6bfc0f4cd4c4ee909a767bdd1540dfee0959f
liujiasheng <longlongxs@gmail.com>
2018-03-13 09:46:50 +0800

    add canBorrow column

--- server/src/class/book/Book.php
+++ server/src/class/book/Book.php
@@ -28,10 +28,14 @@ class Book extends AbstractModel
 
         $builder = $this->capsule->table('book AS b')
             ->leftJoin('book_share AS s', 's.book_id', '=', 'b.id')
+            ->leftJoin('book_borrow AS bb', 'bb.book_share_id', '=', 's.id')
             ->select('b.id','b.isbn10','b.isbn13','b.title','b.image','s.share_status','s.lend_status')
             ->selectRaw('count('.$this->capsule->getConnection()->getTablePrefix().'s.id) AS book_share_sum')
+            ->selectRaw('count('.$this->capsule->getConnection()->getTablePrefix().'bb.id) AS book_borrow_sum')
             ->where('s.share_status', 1)
-            ->where('s.lend_status', 1);
+            ->where(function($q) {
+                $q->where('bb.return_status', 0)->orWhereNull('bb.return_status');
+            });
         if ($name) {
             $builder->where(function ($q) use ($name) {
                 $q->where('b.title', 'like', "%{$name}%")
@@ -40,10 +44,25 @@ class Book extends AbstractModel
                     ->orWhere('b.tags', 'like', "%{$name}%");
             });
         }
+        $totalCount = $builder->count('b.id');
         $builder->groupBy('b.id')
             ->orderBy('s.id', 'desc')
             ->limit($pagesize)->offset($offset);
-        $res['data']['list'] = $builder->get();
+        $data = $builder->get();
+        if (!empty($data)) {
+            $newData = [];
+            foreach ($data as $item) {
+                $item['canBorrow'] = $item['book_share_sum'] > $item['book_borrow_sum'] ? 1 : 0;
+                $newData[] = $item;
+            }
+            $data = $newData;
+        }
+        $res['data'] = [
+            'list' => $data,
+            'total' => intval($totalCount),
+            'pagesize' => $pagesize,
+            'totalpage' => ceil($totalCount / $pagesize),
+        ];
         return $res;
     }
 
