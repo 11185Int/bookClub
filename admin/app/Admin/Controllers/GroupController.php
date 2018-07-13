40f39879965ae6b10cd0af8ef96d113f6ff49e67
liujiasheng <longlongxs@gmail.com>
2018-07-13 10:24:21 +0800

    admin

--- admin/app/Admin/Controllers/GroupController.php
+++ admin/app/Admin/Controllers/GroupController.php
@@ -41,7 +41,16 @@ class GroupController extends Controller
     {
         return Admin::grid(Group::class, function (Grid $grid) {
 
-            $grid->model()->where('group_amount', '>', 0);
+            $model = $grid->model();
+            $model->select('group.id', 'group.group_name', 'group.group_amount', 'user.nickname',
+                'group.create_time', 'group.summary')
+                ->selectRaw('count(tb_bs.id) AS book_amount');
+            $model->where('group_amount', '>', 0);
+            $model->leftJoin('user', 'user.openid', '=', 'group.creator_openid');
+            $model->leftJoin('book_share AS bs', function($join) {
+                $join->on('bs.group_id', '=', 'group.id');
+                $join->where('bs.share_status', '=', 1);
+            })->groupBy('group.id');
 
             $grid->disableActions()
                 ->disableRowSelector()
@@ -50,9 +59,11 @@ class GroupController extends Controller
             $grid->id('ID')->sortable();
             $grid->column('group_name', '小组名称');
             $grid->column('group_amount', '人数')->sortable();
+            $grid->column('book_amount', '藏书量')->sortable();
             $grid->column('0', '个人/小组')->display(function () {
                 return $this->group_amount > 1? '小组' : '个人';
             });
+            $grid->column('nickname', '创建人');
             $grid->column('create_time', '创建时间')->display(function ($create_time) {
                 return date('Y-m-d H:i:s', $create_time);
             });
